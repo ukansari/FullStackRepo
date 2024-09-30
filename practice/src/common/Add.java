feb4382ba96e798bb7c7961624783848ab8ff7a1
ukansari <ansariumair5461@gmail.com>
2024-09-30 22:50:04 +0530

    localdate commit by umair

--- practice/src/common/Add.java
+++ practice/src/common/Add.java
@@ -1,11 +1,17 @@
 package common;
 
+import java.time.LocalDate;
+
 public class Add {
 
 	public static void main(String[] args) {
 
 		System.out.println("TL Repo"+5+7);
+		System.out.println(LocalDate.now());
 
 	}
 
 }
+
+
+
